292e201b11ae4ded32f574e30b55e158f9448012
Brem <94795529+bremproxy@users.noreply.github.com>
2022-06-29 12:01:30 +0300

    ДЗ 1

---
+++ myapp/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows.Forms;
+using System;
+using Microsoft.VisualBasic;
+
+class firstnameAge
+{
+    static void Main()
+    {
+
+        string name, age1;
+        int year = 2022, age, born, a;
+        name = Interaction.InputBox("Как тебя зовут?");
+        age1 = Interaction.InputBox("Какой год твоего рождения?");
+        bool result = int.TryParse(age1, out born);
+        if (result)
+        {
+           MessageBox.Show("Отлично," + born);
+           age = year - born;
+           name="Получаеться, Вам "+name+" "+age+" лет!";
+           MessageBox.Show(name, "Магия!!!");
+        }
+        else
+        {
+           MessageBox.Show("Ты ввел какую то дичь, давай досвиданья " + born);
+        }
+    }
+
+}
